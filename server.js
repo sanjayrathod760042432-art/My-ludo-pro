b61777c22964a789ffdc1016a339fc63fc40fa1b
sanjayrathod760042432-art <sanjayrathod760042432@gmail.com>
2026-09-05 13:53:07 +0530

    Refactor server listen log message format

--- server.js
+++ server.js
@@ -117,6 +117,8 @@ io.on('connection', socket=>{
     }
   });
 });
+const PORT = process.env.PORT || 3000;
 
-const PORT=process.env.PORT||3000;
-server.listen(PORT,()=>console.log(`My Ludo backend running on port ${PORT}`));
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
